6a25d6259a3cd71997a2c1be89450bac02b4edf2
Bradley Stovall <bradleystovall@Stovalls-MBA.local>
2017-03-12 08:18:54 -0800

    Change with first ViewController

---
+++ Do It/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Do It
+//
+//  Created by Bradley Stovall on 3/11/17.
+//  Copyright © 2017 Bradley Stovall. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
