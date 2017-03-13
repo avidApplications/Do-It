9d5a9677aa53567d41bd5bbc57a92f7c440be915
Bradley Stovall <bradleystovall@Stovalls-MBA.local>
2017-03-13 15:31:15 -0800

    Added connection from text field and add button to code.

---
+++ Do It/CreateTaskViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreateTaskViewController.swift
+//  Do It
+//
+//  Created by Bradley Stovall on 3/13/17.
+//  Copyright © 2017 Bradley Stovall. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+    @IBOutlet weak var taskNameTextField: UITextField!
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+   }
