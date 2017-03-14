ea66528d7edb9a4c96276b3a2759a63939ad7f85
Bradley Stovall <bradleystovall@Stovalls-MBA.local>
2017-03-13 16:03:56 -0800

    Changes to add screen so it will pop back to first tableView.

--- Do It/TasksViewController.swift
+++ Do It/TasksViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TasksViewController.swift
 //  Do It
 //
 //  Created by Bradley Stovall on 3/11/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -61,6 +61,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBAction func plusTapped(_ sender: AnyObject) {
         
         performSegue(withIdentifier: "addSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as!
+            CreateTaskViewController
+        nextVC.previousVC = self
+        
+        
     }
     
 
